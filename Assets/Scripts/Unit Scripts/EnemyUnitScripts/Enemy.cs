5c24139062baad0cc3a44aaabd1c1248cfa332fb
Ronald Jon Christopher Bandalaria <ronaldjonchristopher.bandalaria@benilde.edu.ph>
2022-01-24 20:37:34 +0800

    Added waypoints to MapScene. Implemented prototype for simple pathfinding

--- Assets/Scripts/Unit Scripts/EnemyUnitScripts/Enemy.cs
+++ Assets/Scripts/Unit Scripts/EnemyUnitScripts/Enemy.cs
@@ -5,20 +5,55 @@ using UnityEngine;
 public class Enemy : Unit
 {
     [Header("Enemy AI Movement")]
-    List<GameObject> waypoints;
+    public List<GameObject> waypoints;
+    [SerializeField]
     int currentWaypoint = 0;
-    float minDistanceToWaypoint = 0.1f; 
+    public float minDistanceToWaypoint = 0.1f;
+    public float movementSpeed = 1f;
+    [SerializeField]
+    Rigidbody rigidBody;
     // Start is called before the first frame update
     void Start()
     {
-        
+        rigidBody = GetComponent<Rigidbody>();  
+        currentWaypoint = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
+        MoveToWaypoint();
+    }
+
+    private void FixedUpdate()
+    {
         
     }
 
-    void MoveToWaypoint() { }
+    void MoveToWaypoint() {
+        if(currentWaypoint < waypoints.Count)
+        {
+            if (!IsEnemyNearWaypoint())
+            {
+                Debug.Log(waypoints[currentWaypoint].transform.position);
+                this.gameObject.transform.LookAt(waypoints[currentWaypoint].transform);
+                //rigidBody.MovePosition(this.gameObject.transform.position);
+                this.gameObject.transform.position = Vector3.MoveTowards(this.gameObject.transform.position, waypoints[currentWaypoint].transform.position, movementSpeed * Time.deltaTime);
+               
+            }
+            else
+            {
+                currentWaypoint++;
+            }
+            
+        }
+        
+        
+    }
+    bool IsEnemyNearWaypoint()
+    {
+        float distance = Vector3.Distance(rigidBody.transform.position, waypoints[currentWaypoint].transform.position);
+        Debug.Log("Distance between enemy and waypoint: " + distance);
+        return distance <= minDistanceToWaypoint; 
+    }
 }
