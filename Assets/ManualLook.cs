028ef84bdb5be0d2fda2f0845e0075df41729f53
Ronald Jon Christopher Bandalaria <ronaldjonchristopher.bandalaria@benilde.edu.ph>
2022-03-21 22:49:43 +0800

    Cleaned up code for ManualLook

--- Assets/ManualLook.cs
+++ Assets/ManualLook.cs
@@ -32,17 +32,9 @@ public class ManualLook : MonoBehaviour
 
             YRotation += mouseX;
 
-            
-           
-           //TurretHead.rotation = Quaternion.Euler(XRotation, TurretHead.rotation.y * mouseY, 0f);
             this.gameObject.transform.rotation = Quaternion.Euler(XRotation, YRotation, 0f);
             TurretHead.transform.rotation = this.gameObject.transform.rotation;
-            //TurretHead.transform.localRotation = this.gameObject.transform.localRotation;
-            //TurretHead.Rotate(Vector3.up * mouseX);
-            //TurretHead.Rotate(new Vector3(XRotation, 0, 0f));
-            Debug.Log("Turret Head Rotation: " + TurretHead.rotation);
-            //TurretHead.localRotation = Quaternion.Euler(XRotation, 1f * mouseX, 0);
-            //TurretHead.Rotate(new Vector3(-mouseY, mouseX, 0f));
+           
         }
         
     }
